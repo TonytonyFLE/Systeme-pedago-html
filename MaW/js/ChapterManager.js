3f33c0bb7ef0ce7ed3c4697e4d76814ee81fc263
TonytonyFLE <antoine.voglino@gmail.com>
2025-08-28 04:41:39 +0200

    Update ChapterManager.js

--- MaW/js/ChapterManager.js
+++ MaW/js/ChapterManager.js
@@ -911,6 +911,13 @@ this.conversions = {
             this.hidePalette();
         }
     });
+
+        convertToSuperscript(number) {
+    const superscriptMap = {
+        '0': '⁰', '1': '¹', '2': '²', '3': '³', '4': '⁴', '5': '⁵',
+        '6': '⁶', '7': '⁷', '8': '⁸', '9': '⁹', '-': '⁻', '+': '⁺'
+    };
+    return number.split('').map(digit => superscriptMap[digit] || digit).join('');
 }
     
     showPalette(input) {
