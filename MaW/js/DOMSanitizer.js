262c4598414ef2994029838210de533d28af98c1
TonytonyFLE <antoine.voglino@gmail.com>
2025-08-28 10:33:06 +0200

    Create DOMSanitizer.js

---
+++ MaW/js/DOMSanitizer.js
@@ -0,0 +1,46 @@
+/**
+ * Utilitaires pour sécuriser l'injection de contenu dans le DOM
+ */
+class DOMSanitizer {
+    /**
+     * Nettoie une chaîne de caractères pour éviter les injections XSS
+     */
+    static sanitizeText(text) {
+        if (typeof text !== 'string') return '';
+        
+        return text
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#x27;');
+    }
+    
+    /**
+     * Crée un élément DOM sécurisé avec du texte
+     */
+    static createTextElement(tagName, textContent, className = '') {
+        const element = document.createElement(tagName);
+        element.textContent = textContent; // Sécurisé automatiquement
+        if (className) {
+            element.className = className;
+        }
+        return element;
+    }
+    
+    /**
+     * Valide qu'une chaîne ne contient pas de balises HTML dangereuses
+     */
+    static isTextSafe(text) {
+        const dangerousPatterns = [
+            /<script/i,
+            /<iframe/i,
+            /<object/i,
+            /<embed/i,
+            /javascript:/i,
+            /on\w+\s*=/i
+        ];
+        
+        return !dangerousPatterns.some(pattern => pattern.test(text));
+    }
+}
